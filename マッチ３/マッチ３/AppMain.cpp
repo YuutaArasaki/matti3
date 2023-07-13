7d743d3eabd9eb8649e3a9ceaa913e26ce93eebb
新崎雄大 <138739177+YuutaArasaki@users.noreply.github.com>
2023-07-13 12:09:19 +0900

    マッチ３制作
    プログラムを書き込んだ

--- マッチ３/マッチ３/AppMain.cpp
+++ マッチ３/マッチ３/AppMain.cpp
@@ -1,4 +1,31 @@
-#include"DxLib.h" 
+/*****************************************
+
+*プログラミング実習
+
+*マッチ3ゲームの製作
+
+******************************************/
+
+
+#include "DxLib.h" 
+
+#include "FreamControl.h"
+
+#includ "InputControl.h"
+
+#includ "SceneManager.h"
+
+/******************************************
+ 
+ *マクロ定義
+
+ ******************************************/
+
+#define SCREEN_HEIGHT　　(480)    //スクリーンサイズ　(高さ)
+
+#define SCREEN_WIDTH　　 (640)    //スクリーンサイズ　(幅)
+
+#define SCREEN_COLORBIT　(32)     //スクリーンカラービット
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	//ウィンドウモードで起動
