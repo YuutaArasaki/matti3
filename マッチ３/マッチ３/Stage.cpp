30134c31524c04260233c07a6a1122e05f64ef5f
新崎雄大 <138739177+YuutaArasaki@users.noreply.github.com>
2023-07-20 09:32:53 +0900

    cppファイル
    プログラムを記述した

---
+++ マッチ３/マッチ３/Stage.cpp
@@ -0,0 +1,17 @@
+#include "Stage.h"
+
+#include "DxLib.h"
+
+#include "InputControl.h"
+
+/************************************
+
+*マクロ定義
+
+*************************************/
+
+#define HEIGHT              (12)    //ブロック配置サイズ (高さ)
+
+#define WIDTH               (12)    //ブロック配置サイズ (幅)
+
+#define BLOCKSIZE           (48)    //ブロックサイズ
